4e08f278bfceceef9d8397ed804b914bf50bda49
Krasin0880 <83248258+Krasin0880@users.noreply.github.com>
2022-12-28 18:46:56 +0200

    add normal calculation

--- main.js
+++ main.js
@@ -7,35 +7,39 @@ let spaceball;                  // A SimpleRotator object that lets the user rot
 
 function deg2rad(angle) {
     return angle * Math.PI / 180;
-}
-
-let X = (u,v) => u;
-let Y = (u,v) => v;
-let Z = (u,v) => ((1./3 * Math.pow(u,3)) - (1./2 * Math.pow(v,2)));
-// Constructor
+}//Constructor
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
+    this.iNormalBuffer = gl.createBuffer();
     this.count = 0;
 
-    this.BufferData = function(vertices) {
+    this.BufferData = function (vertices) {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
-        this.count = vertices.length/3;
+        this.count = vertices.length / 3;
     }
 
-    this.Draw = function() {
-        let N = 40;
+    this.NormalBufferData = function (normals) {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STREAM_DRAW);
+
+        this.count = normals.length / 3;
+    }
+
+    this.Draw = function () {
+
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
-        for(let i = 0; i <= (N+1) * 2; i++){
-            gl.drawArrays(gl.LINE_STRIP, i*N, N);
-        }
-       
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribNormal);
 
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
     }
 }
 
@@ -48,12 +52,15 @@ function ShaderProgram(name, program) {
 
     // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
+    this.iAttribNormal = -1;
     // Location of the uniform specifying a color for the primitive.
     this.iColor = -1;
     // Location of the uniform matrix representing the combined transformation.
     this.iModelViewProjectionMatrix = -1;
+    this.iNormalMatrix = -1;
+    
 
-    this.Use = function() {
+    this.Use = function () {
         gl.useProgram(this.prog);
     }
 }
@@ -63,73 +70,154 @@ function ShaderProgram(name, program) {
  * (Note that the use of the above drawPrimitive function is not an efficient
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
-function draw() { 
-    gl.clearColor(0,0,0,1);
+function draw() {
+    gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
-    
+    let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
+
     /* Get the view matrix from the SimpleRotator object.*/
     let modelView = spaceball.getViewMatrix();
 
-    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
-    let translateToPointZero = m4.translation(0,0,-10);
+    let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
+    let translateToPointZero = m4.translation(0, 0, -10);
 
-    let matAccum0 = m4.multiply(rotateToPointZero, modelView );
-    let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
-        
+    let matAccum0 = m4.multiply(rotateToPointZero, modelView);
+    let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
+
     /* Multiply the projection matrix times the modelview matrix to give the
        combined transformation matrix, and send that to the shader program. */
-    let modelViewProjection = m4.multiply(projection, matAccum1 );
+    let modelViewProjection = m4.multiply(projection, matAccum1);
 
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
-    
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
+
+    let modelviewInv = new Float32Array(16);
+    let normalmatrix = new Float32Array(16);
+    mat4Invert(modelViewProjection, modelviewInv);
+    mat4Transpose(modelviewInv, normalmatrix);
+
+    gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalmatrix);
+
     /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
-
+    gl.uniform4fv(shProgram.iColor, [0.2, 0.8, 0, 1]);
     surface.Draw();
 }
 
-let diapazonUFrom = 0;
-let diapazonUTo = 1;
-let diapazonVFrom = 0;
-let diapazonVTo = 1.5;
-let step = 0.05;
 
-function CreateSurfaceData()
-{
+
+function CreateSurfaceData(norms = false) {
     let vertexList = [];
-    for (let u = diapazonUFrom; u <= diapazonUTo; u += step) {
-        for (let v = diapazonVFrom; v <= diapazonVTo; v += step) {
-            const u0 = u;
-            const v0 = v;
-            const u1 = u+step;
-            const v1 = v+step;
+    let normalsList = [];
 
-            let x0 = X(u0, v0);
-            let y0 = Y(u0, v0);
-            let z0 = Z(u0, v0);
-
-            let xL = X(u0, v1);
-            let yL = Y(u0, v1);
-            let zL = Z(u0, v1);
-
-            let x1 = X(u1, v1);
-            let y1 = Y(u1, v1);
-            let z1 = Z(u1, v1);
-
-            let xR = X(u1, v0);
-            let yR = Y(u1, v0);
-            let zR = Z(u1, v0);
-
-            vertexList.push(x0, z0, y0);
-            vertexList.push(xL, zL, yL);
-            vertexList.push(x1, z1, y1);
-            vertexList.push(xR, zR, yR);
+    let i = -1;
+    let j = -1;
+    let inc = 0.1;
+    while(i<1){
+        if(inc>0){
+        while(j<1){
+            let v1 = shoe(i, j)
+            let v2 = shoe(i + 0.1, j)
+            let v3 = shoe(i, j + 0.1)
+            let v4 = shoe(i + 0.1, j + 0.1);
+            // one triangle
+            vertexList.push(v1.x, v1.y, v1.z);
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            // another triangle
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v4.x, v4.y, v4.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            let v21 = { x: v2.x - v1.x, y: v2.y - v1.y, z: v2.z - v1.z }
+            let v31 = { x: v3.x - v1.x, y: v3.y - v1.y, z: v3.z - v1.z }
+            let n1 = vec3Cross(v21, v31);
+            vec3Normalize(n1);
+            // normals for one 
+            normalsList.push(n1.x, n1.y, n1.z);
+            normalsList.push(n1.x, n1.y, n1.z);
+            normalsList.push(n1.x, n1.y, n1.z);
+            let v42 = { x: v4.x - v2.x, y: v4.y - v2.y, z: v4.z - v2.z };
+            let v32 = { x: v3.x - v2.x, y: v3.y - v2.y, z: v3.z - v2.z };
+            let n2 = vec3Cross(v42, v32);
+            vec3Normalize(n2);
+            // normals for anothers
+            normalsList.push(n2.x, n2.y, n2.z);
+            normalsList.push(n2.x, n2.y, n2.z);
+            normalsList.push(n2.x, n2.y, n2.z);
+            j += inc
         }
     }
+    else{
+        while(j>-1){
+            let v1 = shoe(i, j)
+            let v2 = shoe(i + 0.1, j)
+            let v3 = shoe(i, j + 0.1)
+            let v4 = shoe(i + 0.1, j + 0.1);
+            // one triangle
+            vertexList.push(v1.x, v1.y, v1.z);
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            // another triangle
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v4.x, v4.y, v4.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            let v21 = { x: v2.x - v1.x, y: v2.y - v1.y, z: v2.z - v1.z }
+            let v31 = { x: v3.x - v1.x, y: v3.y - v1.y, z: v3.z - v1.z }
+            let n1 = vec3Cross(v21, v31);
+            vec3Normalize(n1);
+            // normals for one 
+            normalsList.push(n1.x, n1.y, n1.z);
+            normalsList.push(n1.x, n1.y, n1.z);
+            normalsList.push(n1.x, n1.y, n1.z);
+            let v42 = { x: v4.x - v2.x, y: v4.y - v2.y, z: v4.z - v2.z };
+            let v32 = { x: v3.x - v2.x, y: v3.y - v2.y, z: v3.z - v2.z };
+            let n2 = vec3Cross(v42, v32);
+            vec3Normalize(n2);
+            // normals for anothers
+            normalsList.push(n2.x, n2.y, n2.z);
+            normalsList.push(n2.x, n2.y, n2.z);
+            normalsList.push(n2.x, n2.y, n2.z);
+            j += inc
+        }
+    }
+        if(inc>0){
+            inc*=-1
+            j=1
+        }
+        else{
+            inc*=-1
+            j=-1
+        }
+        
+        i += 0.1;
+    }
+
+    if (norms) {
+        return normalsList;
+    }
     return vertexList;
+}
+
+function shoe(x,y){
+    let x1 = x
+    let y1 = y
+    let z = (x**3)/3 + (y**2)/2
+    return { x: x1, y: y1, z: z }
+}
+
+shoe
+
+
+function vec3Cross(a, b) {
+    let x = a.y * b.z - b.y * a.z;
+    let y = a.z * b.x - b.z * a.x;
+    let z = a.x * b.y - b.x * a.y;
+    return { x: x, y: y, z: z }
+}
+
+function vec3Normalize(a) {
+    var mag = Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+    a[0] /= mag; a[1] /= mag; a[2] /= mag;
 }
 
 
