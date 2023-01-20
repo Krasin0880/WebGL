213e63922858d3c77d3787de62f83121771f5720
Krasin0880 <83248258+Krasin0880@users.noreply.github.com>
2023-01-20 10:38:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,59 @@
 'use strict';
 
 let gl;                         // The webgl context.
-let surface;                    // A surface model
+let surface;                   // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+let sphere;
+let userPoint;
+let magnit;
 
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
 
-let X = (u,v) => u;
-let Y = (u,v) => v;
-let Z = (u,v) => ((1./3 * Math.pow(u,3)) - (1./2 * Math.pow(v,2)));
+
 // Constructor
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
+    this.iVertexTextureBuffer = gl.createBuffer();
     this.count = 0;
+    this.textureCount = 0;
 
-    this.BufferData = function(vertices) {
+    this.BufferData = function (vertices) {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
-        this.count = vertices.length/3;
+        this.count = vertices.length / 3;
     }
 
-    this.Draw = function() {
-        let N = 40;
+    this.TextureBufferData = function (vertices) {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexTextureBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+
+        this.textureCount = vertices.length / 2;
+    }
+
+    this.Draw = function () {
+
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
-        for(let i = 0; i <= (N+1) * 2; i++){
-            gl.drawArrays(gl.LINE_STRIP, i*N, N);
-        }
-       
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexTextureBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribVertexTexture, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertexTexture);
 
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
+    }
+
+    this.DrawSphere = function () {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertex);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
     }
 }
 
@@ -48,12 +66,15 @@ function ShaderProgram(name, program) {
 
     // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
-    // Location of the uniform specifying a color for the primitive.
-    this.iColor = -1;
+    this.iAttribVertexTexture = -1;
     // Location of the uniform matrix representing the combined transformation.
     this.iModelViewProjectionMatrix = -1;
+    this.iTMU = -1;
+    this.iUserPoint = -1;
+    this.iMagnit = 1;
+    this.iTranslateSphere = -1;
 
-    this.Use = function() {
+    this.Use = function () {
         gl.useProgram(this.prog);
     }
 }
@@ -63,89 +84,164 @@ function ShaderProgram(name, program) {
  * (Note that the use of the above drawPrimitive function is not an efficient
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
-function draw() { 
-    gl.clearColor(0,0,0,1);
+function draw() {
+    gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
-    
+    let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
+
     /* Get the view matrix from the SimpleRotator object.*/
     let modelView = spaceball.getViewMatrix();
 
-    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
-    let translateToPointZero = m4.translation(0,0,-10);
+    let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
+    let translateToPointZero = m4.translation(0, 0, -10);
 
-    let matAccum0 = m4.multiply(rotateToPointZero, modelView );
-    let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
-        
+    let matAccum0 = m4.multiply(rotateToPointZero, modelView);
+    let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
+
     /* Multiply the projection matrix times the modelview matrix to give the
        combined transformation matrix, and send that to the shader program. */
-    let modelViewProjection = m4.multiply(projection, matAccum1 );
+    let modelViewProjection = m4.multiply(projection, matAccum1);
 
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
-    
-    /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
 
+    gl.uniform1i(shProgram.iTMU, 0);
+    gl.enable(gl.TEXTURE_2D);
+    gl.uniform2fv(shProgram.iUserPoint, [userPoint.x, userPoint.y]);
+    gl.uniform1f(shProgram.iMagnit, magnit);
+    gl.uniform1f(shProgram.iB, -1);
+
+    gl.uniform3fv(shProgram.iTranslateSphere, [-0., -0., -0.])
     surface.Draw();
+    let translate = shoe(map(userPoint.x, 0, 1, -1,1), map(userPoint.y, 0, 1, -1, 1))
+    gl.uniform3fv(shProgram.iTranslateSphere, [translate.x, translate.y, translate.z])
+    gl.uniform1f(shProgram.iB, 1);
+    sphere.DrawSphere();
 }
 
-let diapazonUFrom = 0;
-let diapazonUTo = 1;
-let diapazonVFrom = 0;
-let diapazonVTo = 1.5;
-let step = 0.05;
-
-function CreateSurfaceData()
-{
+function CreateSurfaceData() {
     let vertexList = [];
-    for (let u = diapazonUFrom; u <= diapazonUTo; u += step) {
-        for (let v = diapazonVFrom; v <= diapazonVTo; v += step) {
-            const u0 = u;
-            const v0 = v;
-            const u1 = u+step;
-            const v1 = v+step;
-
-            let x0 = X(u0, v0);
-            let y0 = Y(u0, v0);
-            let z0 = Z(u0, v0);
-
-            let xL = X(u0, v1);
-            let yL = Y(u0, v1);
-            let zL = Z(u0, v1);
-
-            let x1 = X(u1, v1);
-            let y1 = Y(u1, v1);
-            let z1 = Z(u1, v1);
-
-            let xR = X(u1, v0);
-            let yR = Y(u1, v0);
-            let zR = Z(u1, v0);
-
-            vertexList.push(x0, z0, y0);
-            vertexList.push(xL, zL, yL);
-            vertexList.push(x1, z1, y1);
-            vertexList.push(xR, zR, yR);
+    let i = -1;
+    let j = -1;
+    let inc = 0.1;
+    while (i < 1) {
+        while (j < 1) {
+            let v1 = shoe(i, j)
+            let v2 = shoe(i + 0.1, j)
+            let v3 = shoe(i, j + 0.1)
+            let v4 = shoe(i + 0.1, j + 0.1);
+            // one triangle
+            vertexList.push(v1.x, v1.y, v1.z);
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            // another triangle
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v4.x, v4.y, v4.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            j += inc
         }
+        j = -1;
+        i += inc;
+    }
+    return vertexList;
+}
+function CreateSurfaceTextureData() {
+    let vertexList = [];
+    let i = -1;
+    let j = -1;
+    let inc = 0.1;
+    while (i < 1) {
+        while (j < 1) {
+            let u = map(i, -1, 1, 0, 1)
+            let v = map(j, -Math.PI, 1, 0, 1)
+            vertexList.push(u, v)
+            u = map(i + inc, -1, 1, 0, 1)
+            vertexList.push(u, v)
+            u = map(i, -1, 1, 0, 1)
+            v = map(j + inc, -1, 1, 0, 1)
+            vertexList.push(u, v)
+            u = map(i + inc, -1, 1, 0, 1)
+            v = map(j, -1, 1, 0, 1)
+            vertexList.push(u, v)
+            v = map(j + inc, -1, 1, 0, 1)
+            vertexList.push(u, v)
+            u = map(i, -1, 1, 0, 1)
+            v = map(j + inc, -1, 1, 0, 1)
+            vertexList.push(u, v)
+            j += inc
+        }
+        j = -1;
+        i += inc;
+    }
+    return vertexList
+}
+
+function shoe(x, y) {
+    let x1 = x
+    let y1 = y
+    let z = (x ** 3) / 3 + (y ** 2) / 2
+    return { x: x1, y: y1, z: z }
+}
+function map(val, f1, t1, f2, t2) {
+    let m;
+    m = (val - f1) * (t2 - f2) / (t1 - f1) + f2
+    return Math.min(Math.max(m, f2), t2);
+}
+
+function CreateSphereSurface(r = 0.05) {
+    let vertexList = [];
+    let lon = -Math.PI;
+    let lat = -Math.PI * 0.5;
+    while (lon < Math.PI) {
+        while (lat < Math.PI * 0.5) {
+            let v1 = sphereSurfaceDate(r, lon, lat);
+            let v2 = sphereSurfaceDate(r, lon + 0.5, lat);
+            let v3 = sphereSurfaceDate(r, lon, lat + 0.5);
+            let v4 = sphereSurfaceDate(r, lon + 0.5, lat + 0.5);
+            vertexList.push(v1.x, v1.y, v1.z);
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v4.x, v4.y, v4.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            lat += 0.5;
+        }
+        lat = -Math.PI * 0.5
+        lon += 0.5;
     }
     return vertexList;
 }
 
+function sphereSurfaceDate(r, u, v) {
+    let x = r * Math.sin(u) * Math.cos(v);
+    let y = r * Math.sin(u) * Math.sin(v);
+    let z = r * Math.cos(u);
+    return { x: x, y: y, z: z };
+}
 
 /* Initialize the WebGL context. Called from init() */
 function initGL() {
-    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
+    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
     shProgram = new ShaderProgram('Basic', prog);
     shProgram.Use();
 
-    shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribVertexTexture = gl.getAttribLocation(prog, "vertexTexture");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
+    shProgram.iTMU = gl.getUniformLocation(prog, 'TMU');
+    shProgram.iUserPoint = gl.getUniformLocation(prog, 'userPoint');;
+    shProgram.iMagnit = gl.getUniformLocation(prog, 'magnit');
+    shProgram.iTranslateSphere = gl.getUniformLocation(prog, 'translateSphere');
+    shProgram.iB = gl.getUniformLocation(prog, 'b');
 
+    LoadTexture()
     surface = new Model('Surface');
     surface.BufferData(CreateSurfaceData());
+    surface.TextureBufferData(CreateSurfaceTextureData());
+    sphere = new Model('Sphere');
+    sphere.BufferData(CreateSphereSurface())
 
     gl.enable(gl.DEPTH_TEST);
 }
@@ -160,24 +256,24 @@ function initGL() {
  * source code for the vertex shader and for the fragment shader.
  */
 function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader( gl.VERTEX_SHADER );
-    gl.shaderSource(vsh,vShader);
+    let vsh = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vsh, vShader);
     gl.compileShader(vsh);
-    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
+    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
         throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-     }
-    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
+    }
+    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fsh, fShader);
     gl.compileShader(fsh);
-    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
-       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+        throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
     }
     let prog = gl.createProgram();
-    gl.attachShader(prog,vsh);
+    gl.attachShader(prog, vsh);
     gl.attachShader(prog, fsh);
     gl.linkProgram(prog);
-    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
-       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+        throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
     }
     return prog;
 }
@@ -187,16 +283,22 @@ function createProgram(gl, vShader, fShader) {
  * initialization function that will be called when the page has loaded
  */
 function init() {
+    userPoint = { x: 0.7, y: 0.6 }
+    magnit = 1.0;
     let canvas;
     try {
-        canvas = document.getElementById("webglcanvas");
+        let resolution = Math.min(window.innerHeight, window.innerWidth);
+        canvas = document.querySelector('canvas');
         gl = canvas.getContext("webgl");
-        if ( ! gl ) {
+        canvas.width = resolution;
+        canvas.height = resolution;
+        gl.viewport(0, 0, resolution, resolution);
+        if (!gl) {
             throw "Browser does not support WebGL";
         }
     }
     catch (e) {
-        document.getElementById("canvas-holder").innerHTML =
+        document.querySelector('"canvas-holder"').innerHTML =
             "<p>Sorry, could not get a WebGL graphics context.</p>";
         return;
     }
@@ -211,5 +313,54 @@ function init() {
 
     spaceball = new TrackballRotator(canvas, draw, 0);
 
+    draw()
+}
+
+function LoadTexture() {
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    const image = new Image();
+    image.crossOrigin = 'anonymus';
+
+    image.src = "https://github.com/Krasin0880/WebGL/blob/11c678fd49e40c7b9a17c7896cf7af465a703807/krasin.jpg";
+    image.onload = () => {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(
+            gl.TEXTURE_2D,
+            0,
+            gl.RGBA,
+            gl.RGBA,
+            gl.UNSIGNED_BYTE,
+            image
+        );
+        draw()
+    }
+}
+
+onmousemove = (e) => {
+    magnit = map(e.clientX, 0, window.outerWidth, 0, Math.PI)
+    draw()
+};
+window.onkeydown = (e) => {
+    switch (e.keyCode) {
+        case 87:
+            userPoint.y -= 0.01;
+            break;
+        case 83:
+            userPoint.y += 0.01;
+            break;
+        case 65:
+            userPoint.x += 0.01;
+            break;
+        case 68:
+            userPoint.x -= 0.01;
+            break;
+    }
+    console.log(userPoint)
+    userPoint.x = Math.max(0.01, Math.min(userPoint.x, 0.999))
+    userPoint.y = Math.max(0.01, Math.min(userPoint.y, 0.999))
     draw();
 }
